4c3e25e77d0c577de3082be421b2a8299c256059
Angad Sethi <angad@easyagile.com>
2024-05-02 11:10:46 +1000

    Remove unused

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
-import Spinner from "@/components/Spinner"
 import BlogHero from '@/components/BlogHero';
 import {loadBlogPost} from "@/helpers/file-helpers"
-import CircularColorsDemo from "@/components/CircularColorsDemo/index"
 // import DivisionGroupsDemo from "@/components/DivisionGroupsDemo/index"
 import CodeSnippet from "@/components/CodeSnippet/index"
 import styles from './postSlug.module.css';
 import dynamic from 'next/dynamic';
 
-const DivisionGroupsDemo = dynamic(() => import("@/components/DivisionGroupsDemo/index"), { loading: Spinner });
+// const DivisionGroupsDemo = dynamic(() => import("@/components/DivisionGroupsDemo/index"), { loading: Spinner });
 
 export async function generateMetadata({ params }) {
   const post = await loadBlogPost(params.postSlug);
@@ -31,7 +29,7 @@ const {frontmatter, content } = post;
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-      <MDXRemote source={content} components={{pre: CodeSnippet, DivisionGroupsDemo, CircularColorsDemo}} />
+      <MDXRemote source={content} components={{pre: CodeSnippet}} />
       </div>
     </article>
   );
